193f807462c8c67cbc2f29f5ec275a5a61598083
ananya23101 <ananya23102001@gmail.com>
2022-05-12 17:22:39 +0530

    changes

---
+++ src/js.js
@@ -0,0 +1,48 @@
+let apiKey = 'b93ff4fc403576a36fa3ecf7d9987df7';
+
+var cityName;
+var inVal = document.querySelector('.search');
+var outVal = document.querySelector('.card');
+var search = document.querySelector('.search-button')
+let putValue = document.querySelector('.list');
+function fetchApi(cityName){
+   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+   .then(data => data.json())
+   .then(data1 => displayWather(data1));
+}
+ function matchCity(){
+    cityName = this.value;
+    fetchApi(cityName);
+ }
+ 
+ function getInputValue(){
+    var inputVal = inVal.value;
+      cityName = inputVal;
+    fetchApi(cityName);
+}
+function displayWather(data1){
+   var city = data1.name;
+   var desc = data1.weather[0].description;
+    var ico = data1.weather[0].icon;
+    var temperature = data1.main.temp;
+    var humid = data1.main.humidity;
+    var speed = data1.wind.speed;
+   document.querySelector('.card1').style.display = "none";
+  let stuff = `<div class="card mb-12 mr-12">
+   <h1 class="city text-white text-3xl">${city}</h1>
+   <h3 class="temp text-white text-xl">${temperature}°C</h3>
+   <img src="https://openweathermap.org/img/wn/${ico}.png" class="img w-20 h-20">
+   <h5 class="desc text-white text-lg">${desc}</h5>
+   <h5 class="humid text-white text-sm">Humidity: ${humid}%</h5>
+   <h5 class="speed text-white text-sm">Wind Speed: ${speed}km/h</h5>
+   </div>`
+  putValue.insertAdjacentHTML('beforeend',stuff);
+}
+inVal.addEventListener('keypress', function(e){
+   if (e.key === "Enter"){
+        getInputValue();
+   }
+});
+search.addEventListener('click', getInputValue);
+let putvalue = document.querySelector('.list');
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./src/**/*.html',
+  './src/**/*.js',
+  './public/**/*.html',
+  './public/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        primary:  '#45278B',
+        secondary: '#2E335A',
+        third: '#1C1B33',
+        
+     },
+     fontFamily:{
+      bob: ['Nunito'],
+},
+ width:{
+  '128': '30rem',
+ },
+     height: {
+      "40v": "40vh",
+     },
+    },
+  },
+  plugins: [],
+}
